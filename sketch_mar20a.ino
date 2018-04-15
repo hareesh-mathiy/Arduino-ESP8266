84fd6039711029e9a11fdd3ce81c3d9840a5be5b
hareesh-mathiy <hareesh.mathiy@gmail.com>
2018-04-15 15:42:11 -0400

    Initialization speed up and better handling of data.
    Removed response counter and replaced with a check to see if the ESP8266 is ready rather than waiting a fixed amount of time, speeding up the initialization process drastically.  App handles processing of cotrol signals better in addition to sending out response signals.

--- sketch_mar20a.ino
+++ sketch_mar20a.ino
@@ -3,7 +3,7 @@
 const byte rxPin = 2; // Wire this to Tx Pin of ESP8266
 const byte txPin = 3; // Wire this to Rx Pin of ESP8266
 
-int inPin = 7;         // the number of the input pin
+int inPin = 8;         // the number of the input pin
 int outPin = 13;       // the number of the output pin
 int state = HIGH;      // the current state of the output pin
 int reading;           // the current reading from the input pin
@@ -12,7 +12,6 @@ long time = 0;         // the last time the output pin was toggled
 long debounce = 200;   // the debounce time, increase if the output flickers
 
 int initialize = 0;
-int responseCount = 0;
 String inData;
 String outData = "off";
 String received;
@@ -31,116 +30,113 @@ void setup() {
 }
 
 void loop() {
-   // read the state of the pushbutton value:
-   if(overWrite == 0){
-      reading = digitalRead(inPin);
-   }
-  if(initialize == 7 || initialize == 0){
-    if (reading == HIGH && previous == LOW && millis() - time > debounce) {
-      if (state == HIGH){
-        state = LOW;
-        outData = "off";
-      }
-      else {
-        state = HIGH;
-        outData = "on";
-      }
+  // read the state of the pushbutton value:
+  if(overWrite == 0) reading = digitalRead(inPin);
+  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
+    if (state == HIGH) {
+      state = LOW;
+      outData = "off";
       if(overWrite == 0) initialize = 5;
-      time = millis();   
-      overWrite = 0; 
     }
+    else {
+      state = HIGH;
+      outData = "on";
+      if(overWrite == 0) initialize = 5;
+    }
+    overWrite = 0;
+    time = millis();
   }
-  
+
   digitalWrite(outPin, state);
   previous = reading;
 
-  
-  if(initialize == 0){
-    responseCount = 0;
+  inData = ESP8266.readStringUntil('\n');
+  if (inData.indexOf("+IPD") > -1) {
+    received = inData;
+    initialize = 6;
+  }
+
+
+  if (initialize == 0) {
+    inData = "";
     Serial.println("AT...");
     ESP8266.println("AT");
-    while(responseCount < 5){
+    while (inData.indexOf("OK") == -1) {
       inData = ESP8266.readStringUntil('\n');
-      if(inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
-      responseCount++;
+      if (inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
     }
     initialize = 1;
   }
-  if(initialize == 1){
-    responseCount = 0;
+  if (initialize == 1) {
+    inData = "";
     Serial.println("AT+CWMODE=1...");
     ESP8266.println("AT+CWMODE=1");
-    while(responseCount < 5){
+    while (inData.indexOf("OK") == -1) {
       inData = ESP8266.readStringUntil('\n');
-      if(inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
-      responseCount++;
+      if (inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
     }
     initialize = 2;
-  }  
-  if(initialize == 2){
-    responseCount = 0;
+  }
+  if (initialize == 2) {
+    inData = "";
     Serial.println("AT+CWJAP=”esp”,”testpass”...");
     ESP8266.println("AT+CWJAP=\"esp\",\"testpass\"");
-    while(responseCount < 10){
+    while (inData.indexOf("OK") == -1) {
       inData = ESP8266.readStringUntil('\n');
-      if(inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
-      responseCount++;
+      if (inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
     }
     initialize = 3;
-  }  
-  if(initialize == 3){
-    responseCount = 0;
-    Serial.println("AT+CIPSTART=\"TCP\",\"168.235.103.76\",3001");
-    ESP8266.println("AT+CIPSTART=\"TCP\",\"168.235.103.76\",3001");
-    while(responseCount < 10){
+  }
+  if (initialize == 3) {
+    inData = "";
+    Serial.println("AT+CIPSTART=\"TCP\",\"168.235.103.76\",3002");
+    ESP8266.println("AT+CIPSTART=\"TCP\",\"168.235.103.76\",3002");
+    while (inData.indexOf("OK") == -1) {
       inData = ESP8266.readStringUntil('\n');
-      if(inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
-      responseCount++;
+      if (inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
     }
     initialize = 4;
-  }  
-  if(initialize == 4){
-    responseCount = 0;
+  }
+  if (initialize == 4) {
+    inData = "";
     Serial.println("AT+CIPSTATUS");
     ESP8266.println("AT+CIPSTATUS");
-    while(responseCount < 10){
+    while (inData.indexOf("OK") == -1) {
       inData = ESP8266.readStringUntil('\n');
-      if(inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
-      responseCount++;
+      if (inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
     }
-    initialize = 5;
-  }  
-  if(initialize == 5){
-    responseCount = 0;
+    initialize = 6;
+  }
+  if (initialize == 5) {
+    inData = "";
     Serial.println("AT+CIPSEND=4");
-    ESP8266.println("AT+CIPSEND=4");   
-    while(responseCount < 20){
+    ESP8266.println("AT+CIPSEND=4");
+    while (inData.indexOf("SEND OK") == -1) {
       inData = ESP8266.readStringUntil('\n');
-      if(inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
-       if(inData.indexOf("+IPD") > -1){
+      if (inData.length() > 1) Serial.println("--- Got reponse from ESP8266: " + inData);
+      if (inData.indexOf("+IPD") > -1) {
         received = inData;
-        responseCount = 20;
-       }
-      if(inData[0] == '>'){
+        inData = "SEND OK";
+      }
+      if (inData[0] == '>') {
         Serial.println(outData);
         ESP8266.println(outData);
       }
-      responseCount++;
     }
     initialize = 6;
-  }  
-  if(initialize == 6){
+  }
+  if (initialize == 6) {
     Serial.println("Done");
     initialize = 7;
-    if(received.length() > 1) Serial.println("--- Got reponse from server: " + received);
-    if(received.indexOf("on") > -1){
+    if (received.length() > 1) Serial.println("--- Got reponse from server: " + received);
+    if (received.indexOf("on") > -1) {
       overWrite = 1;
       reading = HIGH;
-       previous = LOW;
+      previous = LOW;
       state = LOW;
       Serial.println("Turned LED on");
     }
-    else if(received.indexOf("off") > -1){
+    else if (received.indexOf("off") > -1) {
       overWrite = 1;
       reading = HIGH;
       previous = LOW;
